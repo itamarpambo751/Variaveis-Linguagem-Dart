093e9fe3f5b3205727c9b3a4e599942dffde8e62
itamarpambo <itamarpambo751@gmail.com>
2023-10-29 18:19:44 +0100

    adicionando outros tipos e suas descrições

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 void main() {
 
-  int id = 8;
+  int id = 8; //Tipo inteiro
 
+  double variavel = 1.24444;// Já sabes
 
+  String nome = "Pendrive"; // Já sabes
+
+  bool estado = false; //...
+
+  var naoSeiOqueE = 1; // Atribui uma tipagem com base no valor inicial e não pode ser alterado o tipo de conteudo depois
+
+  dynamic var2 = 1;//pode mudar de tipo durante a execução do programa
   //print(id);
 }
